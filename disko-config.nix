90afb11898a587aebbc0de43233342e60708264c
Maor Haimovitz <maor@mcsimw.com>
2024-02-18 14:10:37 -0500

    more formatting fixes

--- disko-config.nix
+++ disko-config.nix
@@ -2,20 +2,20 @@
   disko.devices = {
     disk = {
       x = {
-	device = builtins.elemAt disks 0;
-	content = {
-	  type = "gpt";
-	  partitions = {
-	    LEMON = {
-	      size = "100%";
-	      content = {
-	        type = "filesystem";
-		format = "xfs";
-		mountpoint = "/mnt/d";
-	      };
-	    };
-	  };
-	};
+        device = builtins.elemAt disks 0;
+        content = {
+          type = "gpt";
+          partitions = {
+            LEMON = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "xfs";
+                mountpoint = "/mnt/d";
+              };
+            };
+          };
+        };
       };
     };
   };
