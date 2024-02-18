8c4cf1f663749d9202a185e43ee2f4a5303a00da
Maor Haimovitz <maor@mcsimw.com>
2024-02-18 02:41:44 -0500

    added a few things

--- configuration.nix
+++ configuration.nix
@@ -6,8 +6,13 @@
       ./hardware-configuration.nix
     ];
 
-
   services.zfs.autoScrub.enable = true;
+
+  nix = {
+    package = pkgs.nixUnstable;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
@@ -39,12 +44,29 @@
     };
   };
 
-  programs.neovim.enable = true;
+  programs = {
+    neovim.enable = true;
+    git.enable = true;
+    tmux.enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     wget
   ];
 
   services.openssh.enable = false;
+  
+  security = {
+    sudo.enable = false;
+    doas = {
+      enable = true;
+      extraRules = [{
+        users = [ "mcsimw" ];
+	noPass = true;
+	keepEnv = true;
+      }];
+    };
+  };
 
   system.stateVersion = "24.05";
 }
