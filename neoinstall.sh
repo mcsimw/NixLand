043cf4b367ae6275bd852f0ff3b76b8649c17863
Maor Haimovitz <maor@mcsimw.com>
2024-02-29 22:23:58 +0000

    added more stuff

--- neoinstall.sh
+++ neoinstall.sh
@@ -1,1 +1,1 @@
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disko-config.nix
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disko-config.nix && doas nixos-install --no-root-passwd --flake .#failbox
