6066e131dd4b1c402a65ac26d2fa12555325881b
Maor Haimovitz <maor@mcsimw.com>
2024-03-08 09:45:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./compootuers/failbox/disko-config.nix && sudo nixos-install --no-root-passwd --flake .#failbox
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./compootuers/failbox/disko-config.nix && chown 1000:100 /mnt/mnt/c && sudo nixos-install --no-root-passwd --flake .#failbox
